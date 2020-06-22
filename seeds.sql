5e36b8757dfee1465efb4ead76c1147016ab1ec2
rf-spuds <rfspuds@gmail.com>
2020-06-22 15:59:37 -0400

    commit number one; skeleton of app

---
+++ seeds.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS cms_DB;
+CREATE database cms_DB;
+
+USE cms_DB;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role ( 
+	id INT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL, 
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+    );
+    
+    CREATE TABLE employee ( 
+	id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    #FOREIGN KEY (manager_id) REFERENCES role(title)
+    );
+    
